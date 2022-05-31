43b3a3c5338b3d29270f03c75d845d7cb4168291
MunoLike <sukorice@gmail.com>
2022-05-31 15:40:18 +0900

    環境構築（フォーマッタ、venv、setup.ps1）

--- read.py
+++ read.py
@@ -5,25 +5,23 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    wavf='samples\sample_Q_202205\sample_Q_E01\problem.wav'
-    with wave.open(wavf, 'r') as wr:
-        ch=wr.getnchannels()
+    wavf = "samples\sample_Q_202205\sample_Q_E01\problem.wav"
+    with wave.open(wavf, "r") as wr:
+        ch = wr.getnchannels()
         width = wr.getsampwidth()
-        fr=wr.getframerate()
-        fn=wr.getnframes()
+        fr = wr.getframerate()
+        fn = wr.getnframes()
 
-        print(f'ch: {ch}, width: {width}, fr: {fr}, fn: {fn}')
+        print(f"ch: {ch}, width: {width}, fr: {fr}, fn: {fn}")
 
-        data=wr.readframes(wr.getnframes())
-        wav_data=np.frombuffer(data, dtype=np.int16)
+        data = wr.readframes(wr.getnframes())
+        wav_data = np.frombuffer(data, dtype=np.int16)
 
-    timeline=np.arange(0, fn)
+    timeline = np.arange(0, fn)
 
     plt.plot(timeline, wav_data)
     plt.show()
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
