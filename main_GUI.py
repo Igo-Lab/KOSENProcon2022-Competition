1c734745301c46259c7c351d36cc83682a31d1d6
BATOER-0605 <dougahsk@outlook.com>
2022-08-26 01:48:49 +0900

    回答送信時の修正画面の準備（未完成）

--- main_GUI.py
+++ main_GUI.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
 import subprocess
+import tkinter
+from tokenize import String
 import call_test
 import numpy as np
+from functools import partial
 
 window_title = '音声解析の鬼 (GUIテスト)'
 
@@ -18,29 +21,48 @@ def call_main_prog():
     prb_data , sum_list = call_test.test()
     print('完了')
 
-    send_config(prb_data,sum_list)
+    send_ans(prb_data,sum_list)
 
-def send_config(data,list):
+#回答送信の確認画面
+def send_ans(data,list):
     wid = tk.Tk()
 
     wid.geometry('500x250')
     wid.title('回答送信確認画面')
 
+    #判別結果の画面出力と、修正画面
+    #自動判別結果側
+    box_name1 = tk.Label(wid,text='判別結果')
+    box_name1.place(x=50,y=100)
+    box_ans = tk.Entry(wid,width=50)
+    box_ans.place(x=115,y=100)
+    box_ans.insert(tk.END,str(data))
+    #修正入力側
+    box_name2 = tk.Label(wid,text='修正を入力')
+    box_name2.place(x=50,y=120)
+    box_fix = tk.Entry(wid,width=50)
+    box_fix.place(x=115,y=120)
+
     #送信ボタン
-    send_btn = tk.Button(wid,text='修正せずに回答を送信',command=post_ans)
+    send_btn = tk.Button(wid,text='修正せずに回答を送信',command=partial(post_ans,data))
     send_btn.place(x=50,y=50)
 
 
     #修正した回答の送信ボタン
-    fixed_send_btn = tk.Button(wid,text='修正した内容を送信',command=post_fix_ans)
+    fixed_send_btn = tk.Button(wid,text='修正した内容を送信',command=partial(post_fix_ans,box_fix.get))
     fixed_send_btn.place(x=250,y=50)
+    
 
 
-def post_ans():
+
+def post_ans(answer):
     print('自動判別の回答を送信した')
+    print(answer)
 
-def post_fix_ans():
+def post_fix_ans(answer):
+    
     print('修正済みの回答を送信した')
+    print(answer)
 
 
 frm = tk.Tk()
