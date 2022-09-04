467c2e2a378066b05a5700a48589ddc1e56cac35
MunoLike <sukorice@gmail.com>
2022-09-04 20:07:24 +0900

    全ファイルをスキャンするようにした（E01～J44）

--- poc6-imitate-python.cu
+++ poc6-imitate-python.cu
@@ -57,29 +57,27 @@ int main() {
     double iStart = cpuSecond();
     // wave読み込み
     AudioFile<short> problem_wave("samples/original/problem4.wav");
+    thrust::host_vector<short> problem_h(problem_wave.samples[0].begin(), problem_wave.samples[0].end());
     AudioFile<short> baseAudios[BASE_AUDIO_N];
     thrust::host_vector<short> baseAudios_h[BASE_AUDIO_N];
     int baseAudio_length[BASE_AUDIO_N];
 
     for (auto i = 0; i < BASE_AUDIO_N; i++) {
         char buf[1000];
-        sprintf_s(buf, sizeof(buf), "%d.wav", i + 1);
+        snprintf(buf, sizeof(buf), "samples/JKspeech/%d.wav", i + 1);
 
         baseAudios[i].load(buf);
         baseAudio_length[i] = baseAudios[i].getNumSamplesPerChannel();
-        baseAudios_h.resize(baseAudio_length[i]);
+        baseAudios_h[i].resize(baseAudio_length[i]);
         std::copy(baseAudios[i].samples[0].begin(), baseAudios[i].samples[0].end(), baseAudios_h[i].begin());
     }
     printf("%f[s] needed to read problems and baseAudios\n", cpuSecond() - iStart);
 
     problem_wave.printSummary();
-    // ホスト（CPU）側メモリに領域を確保
-    thrust::host_vector<unsigned int> sums_h(problem_length + srcJ01_length - 2);
 
     // デバイス（GPU）側メモリに領域を確保
     int problem_length = problem_wave.getNumSamplesPerChannel();
     thrust::device_vector<short> problem_d(problem_length);
-    thrust::device_vector<unsigned int> sums_d(problem_length + srcJ01_length - 2);
     problem_d = problem_h;
 
     // processing
@@ -92,7 +90,7 @@ int main() {
         dim3 grid((problem_length + baseAudio_length[i] + block.x - 1 - 1) / block.x);
         printf("baseAudio ID: %d, Block: %d, Grid: %d\n", i + 1, block.x, grid.x);
         diffSum<<<grid, block>>>(thrust::raw_pointer_cast(problem_d.data()), thrust::raw_pointer_cast(baseAudio_d.data()), thrust::raw_pointer_cast(sums_d.data()), problem_length, baseAudio_length[i]);
-        auto iter thrust::min_element(sums_d.begin(), sums_d.end());
+        auto iter = thrust::min_element(sums_d.begin(), sums_d.end());
         sums[i].sum = *iter;
         sums[i].id = i;
     }
@@ -100,15 +98,6 @@ int main() {
     for (auto s : sums) {
         printf("ID: %d SUM: %d\n", s.id, s.sum);
     }
-
-    // dim3 block(512);
-    // dim3 grid((problem_length + srcJ01_length + block.x - 1 - 1) / block.x);
-    // printf("Block: %d, Grid: %d\n", block.x, grid.x);
-    // diffSum<<<grid, block>>>(thrust::raw_pointer_cast(problem_d.data()), thrust::raw_pointer_cast(src_d.data()), thrust::raw_pointer_cast(sums_d.data()), problem_length, baseAudio_length[i]);
-
-    // auto iter = thrust::min_element(sums_d.begin(), sums_d.end());
-    // unsigned int pos = iter - sums_d.begin();
-    // std::cout << "MinVal: " << *iter << " pos: " << pos << std::endl;
 
     return 0;
 }
