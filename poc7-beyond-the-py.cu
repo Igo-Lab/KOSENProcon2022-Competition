b8648e231961cc8f6b7f039ddb52d0ff29f04efd
MunoLike <sukorice@gmail.com>
2022-09-04 21:13:45 +0900

    use thrust::reduce

--- poc7-beyond-the-py.cu
+++ poc7-beyond-the-py.cu
@@ -1,9 +1,13 @@
+#include <limits.h>
 #include <sys/time.h>
 #include <thrust/copy.h>
 #include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
 #include <thrust/extrema.h>
+#include <thrust/functional.h>
 #include <thrust/host_vector.h>
 #include <thrust/random.h>
+#include <thrust/reduce.h>
 #include <thrust/transform.h>
 
 #include <algorithm>
@@ -14,6 +18,7 @@
 #include "AudioFile.h"
 
 #define BASE_AUDIO_N (88)
+#define SKIP_N (44)
 
 typedef struct {
     unsigned int sum;
@@ -58,7 +63,6 @@ int main() {
     double iStart = cpuSecond();
     // wave読み込み
     AudioFile<short> problem_wave("samples/original/problem4.wav");
-    thrust::host_vector<short> problem_h(problem_wave.samples[0].begin(), problem_wave.samples[0].end());
     AudioFile<short> baseAudios[BASE_AUDIO_N];
     thrust::host_vector<short> baseAudios_h[BASE_AUDIO_N];
     int baseAudio_length[BASE_AUDIO_N];
@@ -77,20 +81,23 @@ int main() {
     // デバイス（GPU）側メモリに領域を確保
     int problem_length = problem_wave.getNumSamplesPerChannel();
     thrust::device_vector<short> problem_d(problem_length);
-    problem_d = problem_h;
+    thrust::device_vector<short> baseAudios_d[BASE_AUDIO_N];
+    problem_d = problem_wave.samples[0];
 
     // processing
+    for (auto i = 0; i < BASE_AUDIO_N; i++) {
+        //転送
+        baseAudios_d[i] = baseAudios[i].samples[0];
+    }
+
     dim3 block(512);
     for (auto i = 0; i < BASE_AUDIO_N; i++) {
         thrust::device_vector<unsigned int> sums_d(problem_length + baseAudio_length[i] - 2);
-        thrust::device_vector<short> baseAudio_d(baseAudio_length[i]);
-        baseAudio_d = baseAudios[i].samples[0];
 
         dim3 grid((problem_length + baseAudio_length[i] + block.x - 1 - 1) / block.x);
         printf("baseAudio ID: %d, Block: %d, Grid: %d\n", i + 1, block.x, grid.x);
-        diffSum<<<grid, block>>>(thrust::raw_pointer_cast(problem_d.data()), thrust::raw_pointer_cast(baseAudio_d.data()), thrust::raw_pointer_cast(sums_d.data()), problem_length, baseAudio_length[i]);
-        auto iter = thrust::min_element(sums_d.begin(), sums_d.end());
-        sums[i].sum = *iter;
+        diffSum<<<grid, block>>>(thrust::raw_pointer_cast(problem_d.data()), thrust::raw_pointer_cast(baseAudios_d[i].data()), thrust::raw_pointer_cast(sums_d.data()), problem_length, baseAudio_length[i]);
+        sums[i].sum = thrust::reduce(thrust::device, sums_d.begin(), sums_d.end(), UINT_MAX, thrust::minimum<unsigned int>());
         sums[i].id = i;
     }
 
