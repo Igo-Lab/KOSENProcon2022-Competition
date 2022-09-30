fdca46029099703b449fca443f7dbb3fffb1becd
MunoLike <sukorice@gmail.com>
2022-09-30 22:17:23 +0900

    add scipy to requirements.

--- library_use_test/resolver.cu
+++ library_use_test/resolver.cu
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "resolver.h"
 
+void compression(const int16_t *src, ){
+
+}
+
 void resolver(const int16_t *problem, const int16_t **src, const int32_t problem_len, const int32_t *src_length, int32_t **result){
     printf("Success日本語も行ける？🤣\n");
     std::cout << "coutだって呼べちゃう" << std::endl;
