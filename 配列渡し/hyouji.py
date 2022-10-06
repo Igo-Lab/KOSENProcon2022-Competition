3974d51beefb8e79e2c5d4e18e818c6ce3fced94
porosting <p.name_porost009@icloud.com>
2022-10-06 18:37:18 +0900

    追加

--- 配列渡し/hyouji.py
+++ 配列渡し/hyouji.py
@@ -1,8 +1,13 @@
 import ctypes
 
+import numpy as np
 
+dp=ctypes.ndpointer(ctypes.c_int16)
 
+lib=np.cdll.LoadLibrary
 
+lib=ctypes.cdll.LoadLibrary("getbox.so")
 
+post=np.array([1,2,3],[4,5,6],dtype=np.c_int16)
 
-
+post_pp=(post.hairetu["data"][0]+np)
