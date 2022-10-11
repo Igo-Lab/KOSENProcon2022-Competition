a91c28580acb6f49b065049701114cd7d54aee48
porost <p.name_porost009@icloud.com>
2022-10-11 18:12:16 +0900

    first commit

--- test.py
+++
@@ -1,6 +0,0 @@
-import main
-
-print("test.py")
-print(__name__)
-
-print(main.libc.add(1,2))
--- 波形表示するだけ/波形表示3.py
+++
@@ -1,8 +0,0 @@
-import librosa
-import librosa.display
-
-a, sr = librosa.load('E01.wav')
-librosa.display.waveshow(a, sr=sr)
-
-import matplotlib.pyplot as plt
-plt.show()
