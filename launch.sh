90d4569a16e05740257f92b115333a20673b7b23
MunoLike <sukorice@gmail.com>
2022-10-13 13:37:36 +0900

    起動コマンド更新

--- launch.sh
+++ launch.sh
@@ -3,4 +3,6 @@
 source .venv/bin/activate
 python -m kosenprocon
 deactivate
-echo "通信できないときはconfig.iniのトークンとconstant.py内のproxyの値を確認すること．"
+echo "通信できないときは，"
+echo "システム日時の確認をして，ずれてたら設定する(sudo datetimectl set-time "2022-10-13 13:37")"
+echo "config.iniのトークンとconstant.py内のproxyの値を確認すること．"
